7145325d027951814a542d4690b8d77cceac669f
Igor Grammatchikov <igorgrammatchikov@192.168.1.8>
2023-06-09 21:35:09 +0300

    2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,10 +1,14 @@
 #!/usr/bin/env node
 
 const { Command } = require('commander');
+
 const program = new Command();
 
 program
-	.description('Compares two configuration files and shows a difference.')
-	.version('0.0.1');
+
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.0.1')
+  .arguments('<filepath1>, <filepath2>')
+  .option('-f, --format <type>', 'output format');
 
 program.parse();
