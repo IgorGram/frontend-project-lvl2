53e0a0e65d093710e15cd0035f7322763d836dc2
Igor Grammatchikov <igorgrammatchikov@192.168.1.10>
2023-10-30 18:43:05 +0300

    fix review

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -9,28 +9,18 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8').trim();
 
-const expectedStylish = readFile('expectedStylish.txt');
-const expectedPlain = readFile('expectedPlain.txt');
-const expectedJson = readFile('expectedJson.json');
+const filesForCheck = [
+  ['file1.json', 'file2.json', 'expectedStylish.txt'],
+  ['file1.yml', 'file2.yml', 'expectedStylish.txt'],
+  ['file1.json', 'file2.json', 'expectedPlain.txt', 'plain'],
+  ['file1.yml', 'file2.yml', 'expectedPlain.txt', 'plain'],
+  ['file1.json', 'file2.json', 'expectedJson.json', 'json'],
+  ['file1.yml', 'file2.yml', 'expectedJson.json', 'json'],
+];
 
-test.each(['json', 'yml'])('gendiff %s in stylish', (format) => {
-  const path1 = getFixturePath(`file1.${format}`);
-  const path2 = getFixturePath(`file2.${format}`);
+test.each(filesForCheck)('gendiff %s and %s in stylish', (file1, file2, expectedResult, format = 'stylish') => {
+  const path1 = getFixturePath(file1);
+  const path2 = getFixturePath(file2);
 
-  expect(gendiff(path1, path2)).toEqual(expectedStylish);
-  expect(gendiff(path1, path2, 'stylish')).toEqual(expectedStylish);
-});
-
-test.each(['json', 'yml'])('gendiff %s in plain', (format) => {
-  const path1 = getFixturePath(`file1.${format}`);
-  const path2 = getFixturePath(`file2.${format}`);
-
-  expect(gendiff(path1, path2, 'plain')).toEqual(expectedPlain);
-});
-
-test.each(['json', 'yml'])('gendiff %s in json', (format) => {
-  const path1 = getFixturePath(`file1.${format}`);
-  const path2 = getFixturePath(`file2.${format}`);
-
-  expect(gendiff(path1, path2, 'json')).toEqual(expectedJson);
+  expect(gendiff(path1, path2, format)).toEqual(readFile(expectedResult));
 });
